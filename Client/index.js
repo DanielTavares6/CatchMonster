fd211cc59c69674660eb120352cc165e77a6fa15
Zohaib Butt <zohaib_b13@hotmail.com>
2020-06-03 21:18:01 +0200

    trying to generate a plane.

--- Client/index.js
+++ Client/index.js
@@ -1,35 +1,42 @@
 var camera, scene, renderer;
 var geometry, material, mesh;
+var myBoxes = new Array();
+
+let PLANE = 60;
+
  
 init();
-animate();
- 
+
+
 function init() {
  
-    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-    camera.position.z = 1;
+    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10000 );
+    
+    camera.position.y = 100;
+    camera.lookAt(0, 0, 0);
+    //camera.position.z = 10;
  
     scene = new THREE.Scene();
- 
-    geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+    
+    geometry = new THREE.BoxGeometry( 1, 1, 1 );
     material = new THREE.MeshNormalMaterial();
- 
-    mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
+
+    for (let x = 0; x < PLANE; x++) {
+        for(let z = 0; z < PLANE; z++) {
+            mesh = new THREE.Mesh( geometry, material );
+            mesh.position.x = (x * PLANE);
+            mesh.position.y = 0;
+            mesh.position.z = (z * PLANE);
+            myBoxes.push(mesh)
+        }
+    }
+
+    for(let i = 0; i < myBoxes.length; i++) {
+        scene.add( myBoxes[i] );
+    }
  
     renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
- 
-}
- 
-function animate() {
- 
-    requestAnimationFrame( animate );
- 
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.02;
- 
     renderer.render( scene, camera );
- 
 }
